19d8b646f35fceb24a3cf2f7ca7381b0db39f1fb
Hue Tran HT <113428757+tranhuec3cx@users.noreply.github.com>
2026-08-30 01:11:41 +0700

    Load LCM movement routes

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 
-// Nạp bổ sung phân hệ LCM ngay trước khi server bắt đầu lắng nghe,
+// Nạp bổ sung các phân hệ LCM ngay trước khi server bắt đầu lắng nghe,
 // giữ nguyên server.js hiện tại để giảm rủi ro ảnh hưởng các phân hệ đang chạy.
 const originalListen = express.application.listen;
 let lcmRegistered = false;
@@ -9,6 +9,7 @@ let lcmRegistered = false;
 express.application.listen = function patchedListen(...args) {
   if (!lcmRegistered) {
     require(path.join(__dirname, "lcm-routes"))(this);
+    require(path.join(__dirname, "lcm-movements-routes"))(this);
     lcmRegistered = true;
   }
   return originalListen.apply(this, args);
