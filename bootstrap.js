5ee0b9c2053363cb0c3699d2b1929cca5f05f9e1
Hue Tran HT <113428757+tranhuec3cx@users.noreply.github.com>
2026-08-29 23:33:14 +0700

    Add LCM bootstrap loader

---
+++ bootstrap.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+
+// Nạp bổ sung phân hệ LCM ngay trước khi server bắt đầu lắng nghe,
+// giữ nguyên server.js hiện tại để giảm rủi ro ảnh hưởng các phân hệ đang chạy.
+const originalListen = express.application.listen;
+let lcmRegistered = false;
+
+express.application.listen = function patchedListen(...args) {
+  if (!lcmRegistered) {
+    require(path.join(__dirname, "lcm-routes"))(this);
+    lcmRegistered = true;
+  }
+  return originalListen.apply(this, args);
+};
+
+require("./server");
