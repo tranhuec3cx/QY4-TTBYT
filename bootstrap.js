a122172fc4263b006a68b86347e570fc1e735c1b
Hue Tran HT <113428757+tranhuec3cx@users.noreply.github.com>
2026-08-30 16:35:35 +0700

    Register deployment health routes before server listen

--- bootstrap.js
+++ bootstrap.js
@@ -1,17 +1,18 @@
 const path = require("path");
 const express = require("express");
 
-// Nạp bổ sung các phân hệ LCM ngay trước khi server bắt đầu lắng nghe,
+// Nạp bổ sung các phân hệ mở rộng ngay trước khi server bắt đầu lắng nghe,
 // giữ nguyên server.js hiện tại để giảm rủi ro ảnh hưởng các phân hệ đang chạy.
 const originalListen = express.application.listen;
-let lcmRegistered = false;
+let extensionsRegistered = false;
 
 express.application.listen = function patchedListen(...args) {
-  if (!lcmRegistered) {
+  if (!extensionsRegistered) {
+    require(path.join(__dirname, "deployment-routes"))(this);
     require(path.join(__dirname, "lcm-routes"))(this);
     require(path.join(__dirname, "lcm-movements-routes"))(this);
     require(path.join(__dirname, "lcm-replacement-routes"))(this);
-    lcmRegistered = true;
+    extensionsRegistered = true;
   }
   return originalListen.apply(this, args);
 };
